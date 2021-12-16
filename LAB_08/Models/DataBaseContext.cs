5b5f41fca2c52eca466c1403ad403b4aa3469aa1
Daniel <73000732+MonnTS@users.noreply.github.com>
2021-12-16 13:26:50 +0100

    second commit

---
+++ LAB_08/Models/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LAB_08.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pet> Pets { get; set; } = null!;
+    }
+}
---
+++ LAB_08/Models/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LAB_08.Models
+{
+    public class Pet
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Name { get; set; }
+
+        [NotNull]
+        public int Age { get; set; }
+
+        [NotNull]
+        public string Type { get; set; }
+
+        public string Owner { get; set; }
+    }
+}
